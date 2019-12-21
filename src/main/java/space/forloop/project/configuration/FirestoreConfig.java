09e949435dcee1ce2c505722730d837b352fad7f
Chris Turner <chris@forloop.space>
2019-12-21 10:16:12 +0000

    feature: configuration for service-account

--- src/main/java/space/forloop/project/configuration/FirestoreConfig.java
+++ src/main/java/space/forloop/project/configuration/FirestoreConfig.java
@@ -5,21 +5,22 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
 
 @Configuration
 public class FirestoreConfig {
 
+  @Value("${firebase.key}")
+  public String firebaseKey;
+
   @Bean
   public FirebaseAuth firebaseAuth() throws IOException {
 
-    final File file = ResourceUtils.getFile("classpath:serviceAccount.json");
-    final FileInputStream serviceAccount = new FileInputStream(file);
+    final FileInputStream serviceAccount = new FileInputStream(firebaseKey);
 
     final FirebaseOptions options =
         new FirebaseOptions.Builder()
