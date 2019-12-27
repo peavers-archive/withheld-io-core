4ba3d00ac45bc4a1f02702269c1a1f084c342465
Chris Turner <peavers@gmail.com>
2019-12-27 14:41:00 +0000

    feature: give comments an ID to delete them with

--- src/main/java/space/forloop/project/domain/Comment.java
+++ src/main/java/space/forloop/project/domain/Comment.java
@@ -1,11 +1,14 @@
 /* Licensed under Apache-2.0 */
 package space.forloop.project.domain;
 
-import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.Instant;
+import java.util.UUID;
 
 /** @author Chris Turner (chris@forloop.space) */
 @Data
@@ -15,6 +18,12 @@ import lombok.NoArgsConstructor;
 public class Comment {
 
   @Builder.Default private long created = Instant.now().toEpochMilli();
+
+  /**
+   * Comments don't exist outside the concept of {@link CodeLine} so they don't have their own ID.
+   * This will generate a fake one to lookup the comments based on it.
+   */
+  @Id @Builder.Default private String id = UUID.randomUUID().toString();
 
   private FirebaseUser firebaseUser;
 
